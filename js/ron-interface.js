1334251d3c650a7a908c88ac8d86ba1369c43a59
Adam Calhoun and Kate Trahan <kate@>
2018-02-15 15:03:15 -0800

    add functional image add(not random image)

--- js/ron-interface.js
+++ js/ron-interface.js
@@ -1,27 +1,56 @@
 import { Ron } from './../js/ron.js';
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#ron').click(function(){
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url =  `http://ron-swanson-quotes.herokuapp.com/v2/quotes`;
-      request.onload = function() {
-        if(this.status === 200) {
-          resolve(request.response)
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
 
-    promise.then(function(response) {
-      console.log(response);
-      const newSentence = response.slice(1, response.length-1);
-      let quote = newSentence;
-      $('.showQuote').text(quote);
-    });
+
+    function ronCall() {
+      return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest();
+        let url =  `http://ron-swanson-quotes.herokuapp.com/v2/quotes`;
+
+        request.onload = function() {
+          if(this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      });
+    }
+
+    function imageCall() {
+      return new Promise(function(resolve, reject){
+        let request = new XMLHttpRequest();
+        let url = `https://api.unsplash.com/photos.random?count=1&client_id=${apiKey}`
+
+        request.onload = function() {
+          if(this.status === 200) {
+            resolve(request.response);
+          } else {
+            reject(Error(request.statusText));
+          }
+        }
+        request.open("GET", url, true);
+        request.send();
+      });
+    }
+
+    Promise.all([ronCall(), imageCall()])
+    .then(function(response) {
+      const quote = response[0];
+      const newSentence = quote.slice(1, quote.length-1);
+      console.log(quote);
+      $('.showQuote').text(newSentence);
+
+      let image = JSON.parse(response[1])[0]["urls"]["regular"];
+      $('.showQuote').append('</br>' + `<img src='${image}'>`);
+    })
+    // .then(function(response) {
+    // });
 
 
 
